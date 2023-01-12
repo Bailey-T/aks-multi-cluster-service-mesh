3f0dc91fea1c4ba60f9984303a7729f9f6fd693e
TomB <44700588+Bailey-T@users.noreply.github.com>
2023-01-12 15:11:30 +0000

    1

--- istio/scripts/08-configure-remote-endpoint-secret.sh
+++ istio/scripts/08-configure-remote-endpoint-secret.sh
@@ -4,7 +4,7 @@
 source ./00-variables.sh
 
 # Create a secret with credentials to allow Istio to access remote Kubernetes API servers in the first cluster
-istioctl x create-remote-secret --context=$aksClusterTwoName --name=$aksClusterTwoName | kubectl --context=$aksClusterOneName apply -f -
+istioctl x create-remote-secret --context=$aksClusterTwoName --name=aks-cluster-two | kubectl --context=$aksClusterOneName apply -f -
 
 # Create a secret with credentials to allow Istio to access remote Kubernetes API servers in the second cluster
-istioctl x create-remote-secret --context=$aksClusterOneName --name=$aksClusterOneName | kubectl --context=$aksClusterTwoName apply -f -
+istioctl x create-remote-secret --context=$aksClusterOneName --name=aks-cluster-one | kubectl --context=$aksClusterTwoName apply -f -
